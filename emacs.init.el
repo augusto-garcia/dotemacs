41a5b7ca397886434ceaebee745c3dc7d54ab82c
Antonio Augusto F Garcia <aafgarci@gmail.com>
2014-07-30 18:05:55 -0300

    Configuring org-mobile to use with the android app

--- emacs.init.el
+++ emacs.init.el
@@ -830,9 +830,9 @@ user."
 
 ;; To use MobileOrg
 ;; Set to the location of your Org files on your local system
-(setq org-directory "~/org")
+(setq org-directory "~/Dropbox/Emacs/org")
 ;; Set to the name of the file where new notes will be stored
-(setq org-mobile-inbox-for-pull "~/org/flagged.org")
+(setq org-mobile-inbox-for-pull "~/Dropbox/Emacs/org/flagged.org")
 ;; Set to <your Dropbox root directory>/MobileOrg.
 (setq org-mobile-directory "~/Dropbox/MobileOrg")
 
