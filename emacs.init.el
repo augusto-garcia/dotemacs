bfc22144b97dd56c5570d9349866558964e0fa1a
augusto-garcia <aafgarci@gmail.com>
2016-10-18 15:38:00 -0200

    Changing theme to subatomic

--- emacs.init.el
+++ emacs.init.el
@@ -1362,7 +1362,7 @@ and set the focus back to Emacs frame"
 (sp-with-modes '(html-mode sgml-mode)
   (sp-local-pair "<" ">"))
 
-(load-theme 'spacemacs-dark t)
+(load-theme 'subatomic t)
 
 ;; To highlight current line
 (global-hl-line-mode 1)
