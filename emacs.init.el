efa1d856917913421db226b0ba9ec8f96e32f113
augusto-garcia <aafgarci@gmail.com>
2017-11-14 23:09:17 -0200

    Configuring shortcuts to bookmarks

--- emacs.init.el
+++ emacs.init.el
@@ -747,6 +747,9 @@ This command does the inverse of `fill-region'."
 (defalias 'ms 'magit-status)
 (defalias 'tm 'git-timemachine)
 (defalias 'lm 'linum-mode)
+(defalias 'sb 'bookmark-set)
+(defalias 'lb 'bookmark-bmenu-list)
+(defalias 'jb 'bookmark-jump)
 
 ;; shortcut to open file .emacs
 (defun dotemacs ()
@@ -1496,6 +1499,9 @@ and set the focus back to Emacs frame"
               ["Edit file as root (C-x F)" find-file-as-root]
               ["Rename file at butter (C-x C-r)" rename-current-buffer-file]
               ["Using dired (C-x d)" dired]
+              ["Set Bookmark (sb)" sb]
+              ["List Bookmarks (lb)" lb]
+              ["Jump to Bookmark (jb)" jb]
               ["Open .emacs (M-x dotemacs)" dotemacs]
               ["Open emacs.init.org (M-x init)" init]
               ["Goto Last Change (C-.)" goto-last-change]
