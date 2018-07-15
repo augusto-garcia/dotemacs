3f3fde8f26c9453a843c0a453baf41d6d60c882b
augusto-garcia <aafgarci@gmail.com>
2018-07-15 17:23:13 -0300

    Changing theme to deeper blue

--- emacs.init.el
+++ emacs.init.el
@@ -1542,7 +1542,7 @@ and set the focus back to Emacs frame"
 (sp-with-modes '(html-mode sgml-mode)
   (sp-local-pair "<" ">"))
 
-(load-theme 'klere t)
+(load-theme 'deeper-blue t)
 ;(require 'kaolin-themes)
 ;(load-theme 'kaolin-light) ; kaolin-light,kaolin-eclipse,kaolin-eclipse,kaolin-ocean,kaolin-tribal
 
